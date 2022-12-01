6855399b49667856ebc28f24f474dbf46a5f8afe
Omar-Nour <omar.syd.n@gmail.com>
2022-12-02 01:21:34 +0200

    Implemented registration and modified login accordlingly to work with hashing passwords

--- TipTopTrips/app.js
+++ TipTopTrips/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const bcrypt = require("bcrypt")
 var path = require('path');
 
 var app = express();
@@ -16,8 +17,32 @@ app.get('/', function(req, res) {
 });
 
 app.get('/registration', function(req, res) {
-  res.render('registration');
+  res.render('registration',{invalidloginerror: ""});
 });
+
+app.post('/registration', function(req, res) {
+  let usern = req.body.username;
+  let passw = req.body.password;
+  if (usern == "" || passw == "") {
+      res.render('registration', {invalidloginerror: "please input a username and a password"});
+      console.log("Invalid regsitration");
+  } else {
+    MongoClient.connect(url, function(err, client) {
+      if (err) throw err;
+      var dbo = client.db("TipTopTrips");
+      dbo.collection("Accounts").find({username: usern}).toArray(function(err, result) {
+        if (err) throw err;
+        if (result.length > 0) { // username is already taken
+          res.render('registration', {invalidloginerror: "username already taken"});
+          console.log("Invalid regsitration");
+        } else { // new username; proceed to register
+          bcrypt.hash(passw, 10, function(err, hash) { // hash password
+            dbo.collection("Accounts").insertOne({username: usern, password: hash})
+          });
+          res.redirect(201, '/');
+    }});
+  })
+}});
 
 app.get('/home', function(req, res) {
   res.render('home');
@@ -29,20 +54,31 @@ var url = "mongodb+srv://networks:hackstack@netwroksdb.oeyeef4.mongodb.net/test"
 app.post('/', function(req, res) {
   let username = req.body.username;
   let password = req.body.password;
-  MongoClient.connect(url, function(err, client) {
-    if (err) throw err;
-    var dbo = client.db("TipTopTrips");
-    dbo.collection("Accounts").find({username: username, password: password}).toArray(function(err, result) {
+  if (username == "" || password == "") {
+    res.render('login', {invalidloginerror: "Please input a username and a password"});
+    console.log("Invalid login");
+  } else {
+    MongoClient.connect(url, function(err, client) {
       if (err) throw err;
-      if (result.length > 0) {
-        res.redirect('/home');
-        console.log("Valid login");
-      } else {
-        res.render('login', {invalidloginerror: "Invalid username or password"});
-        console.log("Invalid login");
-      }
+      var dbo = client.db("TipTopTrips");
+      dbo.collection("Accounts").findOne({username: username}, function(err, result) {
+        if (err || result == undefined) { // name not found
+          res.render('login', {invalidloginerror: "Invalid username or password"});
+          console.log("Invalid login");
+        } else {
+        bcrypt.compare(password, result.password, function(err, result) { // compare stored hash and hashed input 
+          if (!result) { // no match for password hash
+            res.render('login', {invalidloginerror: "Invalid username or password"});
+            console.log("Invalid login");
+          } else {
+            res.redirect('/home');
+            console.log("Valid login");
+            // TODO authenticate
+          }
+        });}
+    });
   });
-})});
+}});
 
 app.timeout = 60000;
 app.listen(3000);
