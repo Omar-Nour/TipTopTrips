732e9f199b8ddb09c1b9e6b1f98b2bc0ecb16940
Mohamed Shamekh <shamekhjr@gmail.com>
2022-12-27 20:52:51 +0200

    Efficiency improvements to add Bali to wanttogo

--- TipTopTrips/app.js
+++ TipTopTrips/app.js
@@ -236,6 +236,7 @@ app.get('/annapurna',function(req,res){
 app.post('/paris', (req, res) => {
   let err_msg = '';
 
+  //check if paris is already in wanttogo list
   if (req.session.user.wanttogo.includes("paris")) {
 	err_msg = "Paris is already in your want to go list";
 	res.render('paris', {err_msg: err_msg});
@@ -259,28 +260,24 @@ app.post('/paris', (req, res) => {
 app.post('/bali', (req, res) => {
     let err_msg = '';
 
-	//connect to database to perform checks or add new destination to wanttogo list
-	MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true}, function(err, client) {
-		if (err) throw err;
-		var dbo = client.db("TipTopTrips");
-		dbo.collection("Accounts").findOne({username: req.session.user.username}, function(err, result) {
+	//check if bali is already in wanttogo list
+	if (req.session.user.wanttogo.includes("bali")) {
+		err_msg = "Bali is already in your want to go list";
+		res.render('bali', {err_msg: err_msg});
+	} else {
+		//connect to database and add destination to wanntogo
+		MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true}, function(err, client) {
 			if (err) throw err;
-			if (result.wanttogo.includes('bali')) { //check if user already added destination to wanttogo list
-				//re-render the page with an appropriate warning if yes
-				console.log("Bali is already in wanttogo list of "+req.session.user.username);
-				err_msg = "Bali is already in your want-to-go list";
+			var dbo = client.db("TipTopTrips");
+			//add bali to wanttogo
+			dbo.collection("Accounts").updateOne({username: req.session.user.username}, {$push: {"wanttogo": "bali"}}, function(err, result) {
+				if (err) throw err;
+				console.log("Added bali to wanttogo list of "+req.session.user.username);
+				err_msg = "Bali is added to yout want-to-go list";
 				res.render('bali', { err_msg: err_msg } );
-			} else {
-				//add bali to wanttogo if this is not the case
-				dbo.collection("Accounts").updateOne({username: req.session.user.username}, {$push: {"wanttogo": "bali"}}, function(err, result) {
-					if (err) throw err;
-					console.log("Added bali to wanttogo list of "+req.session.user.username);
-					err_msg = "Bali is added to yout want-to-go list";
-					res.render('bali', { err_msg: err_msg } );
-				});
-			}
+			});
 		});
-	});
+	}
 });
 
 app.post('/inca', (req, res) => {
